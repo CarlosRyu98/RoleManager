653bb7c0dfb8f0bafa79b40fc0b6fa9eb55df3d7
CarlosRyu98 <ccoderyu@gmail.com>
2024-09-19 13:36:46 +0200

    front clean

--- frontend/RoleManager/src/app/models/domain.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Domain } from './domain.model';
-
-describe('Domain', () => {
-  it('should create an instance', () => {
-    expect(new Domain()).toBeTruthy();
-  });
-});
--- frontend/RoleManager/src/app/models/faction.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Faction } from './faction.model';
-
-describe('Faction', () => {
-  it('should create an instance', () => {
-    expect(new Faction()).toBeTruthy();
-  });
-});
